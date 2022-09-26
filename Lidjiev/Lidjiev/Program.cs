842312588a93105b8236513c184c4292d25189e7
Syar3005 <113889600+Syar3005@users.noreply.github.com>
2022-09-26 16:42:12 +0300

    Add files via upload

---
+++ Lidjiev/Lidjiev/Program.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+
+    namespace Lidjiev
+    { 
+      class MainClass
+      {
+        public static void Main()
+        {
+            Double x, y, z;
+            Console.WriteLine("Введите значение x");
+            x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение y");
+            y = Double.Parse(Console.ReadLine());
+            z = Math.Pow(x, 3) - 2.5 * x * y + Math.Pow(1.78, 2) - 2.5 *y + 1;
+            Console.WriteLine("Ответ: {0}" ,z);
+        }
+      }
+    }
+
+
+
+
+ 
---
+++ Lidjiev/Lidjiev/Program2.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+
+    namespace Lidjiev
+    { 
+      class MainClass2
+      {
+        public static void Main2()
+        {
+            Double a, b, x;
+            Console.WriteLine("Введите значение a");
+            a = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            b = Double.Parse(Console.ReadLine());
+            x = 3.56 * a + Math.Pow (b,3) - 5.8*(Math.Pow(b,2) + 3.8*a - 1.5);
+            Console.WriteLine("Ответ: {0}" ,x);
+        }
+      }
+    }
+
+
+
+
+ 
